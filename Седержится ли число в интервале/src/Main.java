5bf28ab97227bbe975df70a8079a00d49d5ecb04
goro22052 <goro2205@gmail.com>
2017-02-26 13:55:25 +0200

    first two

---
+++ Седержится ли число в интервале/src/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число а:");
+        int a = scanner.nextInt();
+
+
+
+
+
+
+    }
+}
