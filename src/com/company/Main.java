1ef2b5662946353a06eebe164a6ddce348c0b7ae
goro22052 <goro2205@gmail.com>
2017-02-26 13:28:23 +0200

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число а:");
+
+        int a = scanner.nextInt();
+
+        System.out.print("Введите число b:");
+        int b = scanner.nextInt();
+
+
+        int center_number =  10;  //число c которым сравниваем
+
+        if (Math.abs(center_number - a) < (Math.abs(center_number -b )) ) // узнаем разницу чисел и сравниваем их модули
+            System.out.println(a);
+        else
+            System.out.println(b);
+
     }
 }
