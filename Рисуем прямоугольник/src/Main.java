76dab84640687fbfc4a9afda2390e03d171e9a7e
goro22052 <goro2205@gmail.com>
2017-03-12 11:27:36 +0200

    3 4 день недели высокосный год правило треугольника Существует ли пара Минимум из двух чисел Сортировка трех чисел Сравнение имен три числа цель установлена положительное  число положительные и отрицательные числа От 1 до 10 гадание на долларовый счет Таблица умножения Четные числа Рисуем прямоугольник

---
+++ Рисуем прямоугольник/src/Main.java
@@ -0,0 +1,36 @@
+/**
+ Рисуем прямоугольник
+ Ввести с клавиатуры два числа m и n.
+ Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
+
+ Пример: m=2, n=4
+ 8888
+ 8888
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите число m: ");
+        int m = Integer.parseInt(reader.readLine());
+
+        System.out.println("Введите число n: ");
+        int n = Integer.parseInt(reader.readLine());
+
+     for (int m1 = 0; m1 <= m; m1++)
+     {
+        for (int n1 = 0; n1 <= n; n1++)
+        {
+            System.out.print("8");
+        }
+         System.out.println();
+     }
+
+
+    }
+
+}
