7846fe6247799e3be2b1798517832beb2c2236d8
goro22052 <goro2205@gmail.com>
2017-03-12 18:36:42 +0200

    Рисуем линии  Письмо счастья  Достойная оплата труда

---
+++ Достойная оплата труда/src/Main.java
@@ -0,0 +1,18 @@
+import javax.sound.midi.Soundbank;
+
+/**
+Достойная оплата труда
+Используя цикл вывести на экран сто раз надпись:
+«Я никогда не буду работать за копейки. Амиго»
+Каждое значение вывести с новой строки.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 0; i < 100; i++ )
+        {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+
+        }
+    }
+}
