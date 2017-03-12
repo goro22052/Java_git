3ff22b0c49e019a1025417739d50b036709b7c4d
goro22052 <goro2205@gmail.com>
2017-03-12 10:22:26 +0200

    3 4 день недели высокосный год правило треугольника Существует ли пара Минимум из двух чисел Сортировка трех чисел Сравнение имен три числа цель установлена положительное  число положительные и отрицательные числа От 1 до 10 гадание на долларовый счет Таблица умножения

---
+++ Четные числа/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ Четные числа
+ Используя цикл for вывести на экран чётные числа от 1 до 100 включительно.
+ Каждое значение вывести с новой строки.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+        for (int i)
+
+
+    }
+
+}
+
