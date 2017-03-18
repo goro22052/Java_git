1718d97d2d8535e0e30c04cc3945dd39a2323389
goro22052 <goro2205@gmail.com>
2017-03-18 10:43:49 +0200

    Рисуем линии  Письмо счастья  Достойная оплата труда  Как-то средненько  Суммирование

--- Суммирование/src/Main.java
+++ Суммирование/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  Суммирование
  Вводить с клавиатуры числа и считать их сумму.
@@ -7,6 +10,16 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum =0;
+
+        for (int i = 0; i != -1;)
+        {
+            i = Integer.parseInt(reader.readLine());
+            sum+=i;
+        }
+        System.out.println(sum);
 
     }
 }
